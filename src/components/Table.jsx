347cbd4f65eceb3f37f48525de309db70c9e75ca
Italo Sergio <italosergio@graduate.org>
2021-12-08 03:12:12 -0300

    REQ 1 PASS {   update: 'component Table',   progress: 4/4 }

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,12 +3,50 @@ import PlanetsContext from '../context/PlanetsContext';
 
 function Table() {
   const { planets } = useContext(PlanetsContext);
+  const heads = [
+    'Name',
+    'Rotation Period (h)',
+    'Orbital Period (d)',
+    'Diameter (m)',
+    'Climate',
+    'Gravity',
+    'Terrain',
+    'Surface Water (%)',
+    'Population',
+    'Residents',
+    'Created',
+    'Edited',
+    'url',
+  ];
   return (
-    <ul>
+    <table>
+      <thead>
+        <tr>
+          {heads.map((title) => (
+            <th key={ title }>{title}</th>
+          ))}
+        </tr>
+      </thead>
       {planets.map((planet) => (
-        <li key={ planet.name }>{planet.name}</li>
+        <tbody key={ planet.name }q>
+          <tr>
+            <td key={ planet.name }>{planet.name}</td>
+            <td key={ planet.rotation_period }>{planet.rotation_period}</td>
+            <td key={ planet.orbital_period }>{planet.orbital_period}</td>
+            <td key={ planet.diameter }>{planet.diameter}</td>
+            <td key={ planet.climate }>{planet.climate}</td>
+            <td key={ planet.gravity }>{planet.gravity}</td>
+            <td key={ planet.terrain }>{planet.terrain}</td>
+            <td key={ planet.surface_water }>{planet.surface_water}</td>
+            <td key={ planet.population }>{planet.population}</td>
+            <td key={ planet.residents }>{planet.residents}</td>
+            <td key={ planet.created }>{planet.created}</td>
+            <td key={ planet.edited }>{planet.edited}</td>
+            <td key={ planet.url }>{planet.url}</td>
+          </tr>
+        </tbody>
       ))}
-    </ul>
+    </table>
   );
 }
 
