cf2ad1ade119635d7513aeba6bc1cfb2286e8df8
Italo Sergio <italosergio@graduate.org>
2021-12-22 19:56:38 -0300

    { style: 'rotate header icon' }

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../App.css';
 
 export default function Header() {
   return (
     <h1 className="inverted transparent ui center aligned icon header">
-      <i className="inverted circular fighter jet icon" />
+      <i className="inverted circular fighter jet icon rotate-icon" />
       Star Wars
       <div className="sub header">Planet Search</div>
     </h1>
