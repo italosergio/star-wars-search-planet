778c989722a3266c17829cfaefbb8fa5580b2a10
Italo Sergio <italosergio@graduate.org>
2022-01-10 02:55:02 -0300

    { codeReview: 'Leo's review done' }

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
   return (
     <header>
       <h1 className="inverted transparent ui center aligned icon header">
-        <a href="https://github.com/italosergio" className={ `sub header shake ${shakeConstant && 'shake-constant'}` }>A Trybe Project by italosergio</a>
+        <a href="https://github.com/italosergio" className={ `sub header shake-crazy ${shakeConstant && 'shake-constant'}` }>A Trybe Project by italosergio</a>
         <i className="circular fighter jet icon rotate-icon" />
         <div className="header h1 shake-little">STAR WARS</div>
         <div className="sub header shake-little">Planet Search</div>
